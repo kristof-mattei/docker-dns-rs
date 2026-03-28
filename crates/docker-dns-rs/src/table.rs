9c5aec2fb5f7356cb08c59ccf76b2c616571e45e
Kristof Mattei <864376+kristof-mattei@users.noreply.github.com>
2026-03-28 07:48:40 -0700

    fix: rename and remove now handle a & aaaa

--- crates/docker-dns-rs/src/table.rs
+++ crates/docker-dns-rs/src/table.rs
@@ -3,9 +3,9 @@ use std::sync::Arc;
 
 use color_eyre::eyre::Report;
 use hickory_server::proto::rr::rdata::PTR;
-use hickory_server::proto::rr::{Name, RData, RecordSet, RecordType, RrKey};
+use hickory_server::proto::rr::{LowerName, Name, RData, RecordSet, RecordType, RrKey};
 use hickory_server::store::in_memory::InMemoryAuthority;
-use tracing::{Level, event};
+use tracing::{Level, event, instrument};
 
 pub struct AuthorityWrapper {
     authority: Arc<InMemoryAuthority>,
@@ -103,121 +103,151 @@ impl AuthorityWrapper {
     //     Ok(HashSet::new())
     // }
 
-    pub async fn rename(&self, old_name: &str, new_name: &str) -> Result<(), Report> {
-        let old_key = RrKey::new(
-            old_name.strip_prefix('/').unwrap_or(old_name).parse()?,
-            RecordType::A,
-        );
-        let new_name_parsed: Name = new_name.parse()?;
-
-        let mut records = self.authority.records().await;
+    #[instrument(skip_all, fields(old_name = %old_key.name, %new_name, r#type = %old_key.record_type))]
+    async fn rename_records(&self, old_key: &RrKey, new_name: &Name) -> Result<(), ()> {
+        let mut records = self.authority.records_mut().await;
 
         let mut ips: Vec<IpAddr> = vec![];
 
-        if let Some(v) = records.remove(&old_key) {
-            for record in v.records_without_rrsigs() {
-                let data = record.data();
+        let Some(record_set) = records.remove(old_key) else {
+            return Err(());
+        };
 
-                if let Some(a) = data.as_a() {
-                    ips.push(a.0.into());
-                } else if let Some(aaaa) = data.as_aaaa() {
-                    ips.push(aaaa.0.into());
-                } else {
-                    // we only care about A & AAAA
-                }
+        drop(records);
+
+        for record in record_set.records_without_rrsigs() {
+            let data = record.data();
+
+            if let Some(address) = data.ip_addr() {
+                ips.push(address);
+            } else {
+                // we only care about A & AAAA
             }
-        } else {
-            event!(
-                Level::ERROR,
-                "table.rename {} -> {}, entry not found",
-                old_name,
-                new_name
-            );
         }
 
         for ip in ips {
-            self.upsert(new_name_parsed.clone(), ip).await;
+            self.upsert(new_name.clone(), ip).await;
 
             event!(
                 Level::INFO,
-                "table.rename {} -> {} ({})",
-                old_name,
-                new_name,
-                ip
+                %ip,
+                "table.rename",
             );
         }
 
         Ok(())
     }
 
-    pub async fn remove(&self, name: &str) -> Result<(), Report> {
+    pub async fn rename(&self, old_name: &str, new_name: &str) -> Result<(), Report> {
+        let new_name_parsed: Name = new_name.parse()?;
+        let old_key: LowerName = old_name.strip_prefix('/').unwrap_or(old_name).parse()?;
+
+        let old_key_a = RrKey::new(old_key.clone(), RecordType::A);
+        let a_result = self.rename_records(&old_key_a, &new_name_parsed).await;
+
+        let old_key_aaaa = RrKey::new(old_key, RecordType::AAAA);
+        let aaaa_result = self.rename_records(&old_key_aaaa, &new_name_parsed).await;
+
+        if let (Err(()), Err(())) = (a_result, aaaa_result) {
+            event!(
+                Level::WARN,
+                %old_name,
+                %new_name,
+                "No A or AAAA records found"
+            );
+        }
+
+        // this is not fatal
+        Ok(())
+    }
+
+    #[instrument(skip_all, fields(name = %rrkey.name, r#type = %rrkey.record_type))]
+    async fn remove_records(&self, rrkey: &RrKey) -> Result<(), ()> {
         let mut records = self.authority.records_mut().await;
 
-        let rrkey = RrKey::new(name.parse()?, RecordType::A);
-
         // we delete the incoming name -> ip from our storage
-        if let Some(record_set) = records.remove(&rrkey) {
-            let names = record_set
-                .records_without_rrsigs()
-                .filter_map(|record| record.data().as_a())
-                .collect::<Vec<_>>();
+        let Some(record_set) = records.remove(rrkey) else {
+            return Err(());
+        };
 
+        drop(records);
+
+        let records = record_set
+            .records_without_rrsigs()
+            .filter_map(|record| record.data().ip_addr())
+            .collect::<Vec<_>>();
+
+        event!(
+            Level::INFO,
+            ip_addresses = %pretty_print_vec(records.iter()),
+            "table.remove",
+        );
+
+        // and for each ip in name -> ip we delete the PTR record
+        // for address in addresses {
+        //     let ptr_address = AuthorityWrapper::build_reversed(
+        //         &address.parse().expect("Expected IP Address"),
+        //     );
+        //     let ptr_key = self.key(&ptr_address);
+
+        // let raw_entry_builder = storage.raw_entry_mut();
+
+        // match raw_entry_builder.from_key(&ptr_key) {
+        //     RawEntryMut::Occupied(mut o) => {
+        //         let targets = o.get_mut();
+        //         targets.remove(name);
+
+        //         event!(Level::INFO, "table.remove {} -> {}", ptr_key, name);
+
+        //         if targets.is_empty() {
+        //             o.remove();
+
+        //             event!(Level::INFO, "table.remove {} as it is empty", ptr_key);
+        //         }
+        //     },
+        //     RawEntryMut::Vacant(_) => {
+        //         event!(
+        //             Level::WARN,
+        //             "table.remove {} -> {} failed, PTR record not found",
+        //             ptr_key,
+        //             name
+        //         );
+        //     },
+        // }
+
+        // match storage.raw_entry_mut(ptr_key) {
+        //     Entry::Occupied(mut o) => {
+        //         o.get_mut().remove(name);
+        //     },
+        //     Entry::Vacant(_) => {
+        //     },
+        // }
+
+        // if let Some(v) = storage.get_mut(&ptr_key) {
+        //     if v.remove(name) {
+        //         event!(Level::INFO, "table.remove {} -> {}", ptr_key, name);
+        //     } else {
+        //     }
+        // }
+
+        Ok(())
+    }
+
+    pub async fn remove(&self, name: &str) -> Result<(), Report> {
+        let parsed_name: LowerName = name.parse()?;
+
+        let rrkey_a = RrKey::new(parsed_name.clone(), RecordType::A);
+        let a_result = self.remove_records(&rrkey_a).await;
+
+        let rrkey_aaaa = RrKey::new(parsed_name, RecordType::AAAA);
+        let aaaa_result = self.remove_records(&rrkey_aaaa).await;
+
+        if let (Err(()), Err(())) = (a_result, aaaa_result) {
             event!(
-                Level::INFO,
-                "table.remove {} -> {}",
-                name,
-                pretty_print_vec(names.iter())
+                Level::WARN,
+                %name,
+                "No A or AAAA records found"
             );
-
-            // and for each ip in name -> ip we delete the PTR record
-            // for address in addresses {
-            //     let ptr_address = AuthorityWrapper::build_reversed(
-            //         &address.parse().expect("Expected IP Address"),
-            //     );
-            //     let ptr_key = self.key(&ptr_address);
-
-            // let raw_entry_builder = storage.raw_entry_mut();
-
-            // match raw_entry_builder.from_key(&ptr_key) {
-            //     RawEntryMut::Occupied(mut o) => {
-            //         let targets = o.get_mut();
-            //         targets.remove(name);
-
-            //         event!(Level::INFO, "table.remove {} -> {}", ptr_key, name);
-
-            //         if targets.is_empty() {
-            //             o.remove();
-
-            //             event!(Level::INFO, "table.remove {} as it is empty", ptr_key);
-            //         }
-            //     },
-            //     RawEntryMut::Vacant(_) => {
-            //         event!(
-            //             Level::WARN,
-            //             "table.remove {} -> {} failed, PTR record not found",
-            //             ptr_key,
-            //             name
-            //         );
-            //     },
-            // }
-
-            // match storage.raw_entry_mut(ptr_key) {
-            //     Entry::Occupied(mut o) => {
-            //         o.get_mut().remove(name);
-            //     },
-            //     Entry::Vacant(_) => {
-            //     },
-            // }
-
-            // if let Some(v) = storage.get_mut(&ptr_key) {
-            //     if v.remove(name) {
-            //         event!(Level::INFO, "table.remove {} -> {}", ptr_key, name);
-            //     } else {
-            //     }
-            // }
-            // }
-        } else {
-            event!(Level::ERROR, "table.remove {}, entry not found", name);
         }
 
         Ok(())
